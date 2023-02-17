2f7e803d5cd49d8138d91492f074cb7fead4fd0f
XirhxQ <3120210063@bit.edu.cn>
2023-02-17 11:53:18 +0800

    add topic to talker and listener

--- JustZmqListener.py
+++ JustZmqListener.py
@@ -1,10 +1,12 @@
 import zmq
+import struct
 
 context = zmq.Context()
 socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5559")
-socket.setsockopt(zmq.SUBSCRIBE, b'')
+socket.connect("tcp://192.168.43.138:5555")
+socket.setsockopt(zmq.SUBSCRIBE, b'A_to_B')
 
 while True:
     message = socket.recv()
-    print(message)
+    data = struct.unpack('f' * (len(message) // 4), message)
+    print(data)
